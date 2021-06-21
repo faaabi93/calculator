ca89fbd6e1013699c9534b9e320221eaaa3f060f
faaabi93 <fabian.baiersdoerfer@gmx.de>
2021-06-21 22:03:26 +0200

    working calculator

--- script.js
+++ script.js
@@ -4,49 +4,84 @@ const numbersBtns = document.querySelectorAll(".number");
 const equalsBtn = document.querySelector(".equals");
 const percentBtn = document.querySelector("#c4");
 const clearBtn = document.querySelector("#c2");
+const plusMinusBtn = document.querySelector("#c3");
+const pointBtn = document.querySelector(".point");
 
-let firstValue = "";
-let secondValue = "";
-let operant = "";
-let resetDisplay = true;
+clearAll();
 
-numbersBtns.forEach(btn => btn.addEventListener("click", () => {
-    if(resetDisplay) {
-        displayField.textContent = btn.textContent;
-        resetDisplay = false;
-    } else {
-        displayField.textContent += btn.textContent;
+pointBtn.addEventListener("click", () => appendPoint());
+clearBtn.addEventListener("click", () => clearAll());
+equalsBtn.addEventListener("click", () => evaluate());
+percentBtn.addEventListener("click", () => percentage());
+plusMinusBtn.addEventListener("click", () => changeSign());
+
+numbersBtns.forEach(btn => btn.addEventListener("click", () => appendNum(btn.textContent)));
+operatorBtns.forEach(btn => btn.addEventListener("click", () => setOperation(btn.textContent)));
+
+function appendNum(num) {
+    if (displayField.textContent === "0" || resetDisplay) clearDisplay();
+    displayField.textContent += num;
+    clearBtn.textContent  = "C";
+};
+
+function appendPoint() {
+    if (!displayField.textContent.includes(".")){
+        displayField.textContent += ".";
     }
-    clearBtn.textContent = "C";
-}));
+    return;
+}
 
-clearBtn.addEventListener("click", reset);
-
-operatorBtns.forEach(btn => btn.addEventListener("click", () => {
-    if(operant === "") {
-        firstValue = displayField.textContent;
-        operant = btn.textContent;
-        resetDisplay = true;
-    } else {
-        result = operate(firstValue, displayField.textContent, operant);
-        operant = btn.textContent;
-        displayField.textContent = result;
-        resetDisplay = true;
+function evaluate() {
+    secondOperand = displayField.textContent;
+    if(secondOperand === "0" && currentOperation === "/") {
+        alert("You can't divide by 0!")
+        clearAll();
+        return;
     }
-}));
+    displayField.textContent = roundResult(operate(firstOperand, secondOperand, currentOperation));
+    firstOperand = roundResult(operate(firstOperand, secondOperand, currentOperation));
+};
 
-function reset() {
-    displayField.textContent = "0";
-    firstValue = "";
-    secondValue = "";
-    operant = "";
-    clearBtn.textContent = "AC";
+function setOperation(op) {
+    if(currentOperation !== null) {
+        evaluate();
+    }
+    currentOperation = op;
+    console.log(currentOperation);
+    firstOperand = displayField.textContent;
     resetDisplay = true;
 };
+
+function changeSign(){
+    displayField.textContent = parseFloat(displayField.textContent) * (-1);
+}
+
+function clearDisplay() {
+    displayField.textContent = "";
+    resetDisplay = false;
+};
+
+function percentage() {
+    displayField.textContent = displayField.textContent / 100;
+};
+
+function clearAll() {
+    clearBtn.textContent = "AC";
+    displayField.textContent = "0";
+    firstOperand = "";
+    secondOperand = "";
+    currentOperation = null;
+    resetDisplay = false;
+}
+
+function roundResult(num) {
+    return Math.round(num * 1000) / 1000;
+}
 
 function operate(a, b, op) {
     a = parseFloat(a);
     b = parseFloat(b);
+    
     switch (op) {
         case "+":
             return add(a, b);
